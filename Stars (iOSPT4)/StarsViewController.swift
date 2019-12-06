b3f6cf7d5213eda254e9af62197e89f73d19bc68
Johnny Hicks <johnnydhicks@gmail.com>
2019-12-05 20:08:35 -0700

    Set up views and outlets

---
+++ Stars (iOSPT4)/StarsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  StarsViewController.swift
+//  Stars (iOSPT4)
+//
+//  Created by Johnny Hicks on 12/5/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class StarsViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var distanceTextField: UITextField!
+    @IBOutlet var tableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - IBActions
+    @IBAction func printStars(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func createStar(_ sender: UIButton) {
+    }
+    
+    
+
+}
+
